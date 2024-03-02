67da619c14d7359fc6aec87951fdfb9e50a46bc2
alexsokolov <frank9154@gmail.com>
2024-03-02 23:50:39 +0300

    First commit

--- TrafficLightSwiftUI/TrafficLightSwiftUIApp.swift
+++ TrafficLightSwiftUI/TrafficLightSwiftUIApp.swift
@@ -11,7 +11,8 @@ import SwiftUI
 struct TrafficLightSwiftUIApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            //ContentView()
+            TrafficLightView()
         }
     }
 }
